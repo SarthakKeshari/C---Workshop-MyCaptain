f0fdbaf88c1d3d2355b55ed13c01303734bce838
SarthakKeshari <55992140+SarthakKeshari@users.noreply.github.com>
2020-05-08 00:27:58 +0530

    Product of two numbers
    Basics of C++

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //To declare variable a and b
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    //For printing product of a and b
+    cout<<"Product = "<<a*b;
+}
